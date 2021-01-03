2fb92aa360b7b49212ff2081d95eafb29ef6f5a8
Thales Morais de Almeida <thales.morais21@gmail.com>
2021-01-03 16:07:45 -0300

    First COmmit

---
+++ src/components/Navbar.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import github from '../img/github-icon.svg'
+import logo from '../img/logo.svg'
+
+import './global.css';
+
+const Navbar = class extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: false,
+      navBarActiveClass: '',
+    }
+  }
+
+  toggleHamburger = () => {
+    // toggle the active boolean in the state
+    this.setState(
+      {
+        active: !this.state.active,
+      },
+      // after state has been updated,
+      () => {
+        // set the class in state for the navbar accordingly
+        this.state.active
+          ? this.setState({
+            navBarActiveClass: 'is-active',
+          })
+          : this.setState({
+            navBarActiveClass: '',
+          })
+      }
+    )
+  }
+
+  render() {
+    return (
+      <nav
+        className="navbar"
+        role="navigation"
+        aria-label="main-navigation"
+        style={{
+          background: '#18a0fb'
+        }}
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <Link to="/" title="Logo">
+              <img src={logo} alt="Kaldi" style={{ width: '100px', width: '300px' }} />
+            </Link>
+            {/* Hamburger menu */}
+            <div
+              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+              data-target="navMenu"
+              onClick={() => this.toggleHamburger()}
+            >
+              <span />
+              <span />
+              <span />
+            </div>
+          </div>
+          <div
+            id="navMenu"
+            className={`navbar-menu ${this.state.navBarActiveClass}`}
+          >
+            <div className="navbar-start" style={{
+              marginLeft: 'auto',
+
+            }}>
+              <Link className="navbar-item-custom" style={{ color: 'white' }} to="/about">
+                Sobre-nós
+              </Link>
+              <a className="navbar-item-custom" style={{ color: 'white' }} href="https://app.florescereducacao.com.br/buy-speeches">
+                Palestras
+              </a>
+              <Link className="navbar-item-custom" style={{ color: 'white' }} to="/blog">
+                Blog
+              </Link>
+              <Link className="navbar-item-custom" style={{ color: 'white' }} to="/contact">
+                Contato
+              </Link>
+              <Link className="navbar-item-custom" style={{ color: 'white' }} to="/contact/examples">
+                Form Examples
+              </Link>
+            </div>
+            <div className="navbar-end has-text-centered">
+              <a
+                className="navbar-item"
+                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {/* <span className="icon">
+                  <img src={github} alt="Github" />
+                </span> */}
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
---
+++ src/pages/index.md
@@ -0,0 +1,13 @@
+---
+templateKey: index-page
+title: Florescer Educação
+image: /img/home-jumbotron.jpg
+heading: Modelando competencias
+subheading: Support sustainable farming while enjoying a cup
+mainpitch:
+  title: O que é a Florescer Educação
+  description: >
+
+description: >-
+  As abordagens tradicionais de ensino para as crianças estão ultrapassadas. Moldar as crianças para se adaptarem a um sistema tem se mostrado ineficaz. Crianças não são máquinas para aprenderem de forma padronizada. A educação não possui uma fórmula mágica. E cada ser humano possui uma forma diferente de aprender. Por isso, acreditamos no poder da simplicidade e do amor para a constante transformação das pessoas. Afinal, é a nossa capacidade emocional que nos torna mais humanos e menos mecânicos. As palestras da Florescer foram pensadas com muito amor. São vários temas que trazemos para ampliar os conhecimentos e criar novos vínculos entre pais e professores sobre como se engajar com as crianças de hojê.
+---
